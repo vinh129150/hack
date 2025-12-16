ee7121d100aebb6bfebd1e790f4efb735af47b01
vinh129150 <vgg8862@gmail.com>
2025-12-16 15:21:37 +0700

    Update BaconHub.lua

--- BaconHub.lua
+++ BaconHub.lua
@@ -1,12 +1,10 @@
-local Games = {
-    ["76558904092080"] = "https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Theforge.lua",
-    ["121864768012064"] = "https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Fishit.lua",
-}
+local HttpService = game:GetService("HttpService")
+local UniverseID = HttpService:JSONDecode(
+    game:HttpGet("https://apis.roblox.com/universes/v1/places/" .. game.PlaceId .. "/universe")
+).universeId
 
-local gameId = tostring(game.PlaceId)
-
-if Games[gameId] then
-    pcall(function()
-        loadstring(game:HttpGet(Games[gameId]))()
-    end)
+if game.PlaceId == 6701277882 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Fishit.lua"))()
+elseif UniverseID == 7671049560 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Theforge.lua"))()
 end
