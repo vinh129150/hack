8852e8f849e325c00a0939d042832d02ef1703ac
vinh129150 <vgg8862@gmail.com>
2025-12-16 14:51:24 +0700

    Update BaconHub.lua

--- BaconHub.lua
+++ BaconHub.lua
@@ -1,1 +1,12 @@
-hi
+local Games = {
+    ["76558904092080"] = "https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Theforge.lua",
+    ["121864768012064"] = "https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Fishit.lua",
+}
+
+local gameId = tostring(game.PlaceId)
+
+if Games[gameId] then
+    pcall(function()
+        loadstring(game:HttpGet(Games[gameId]))()
+    end)
+end
