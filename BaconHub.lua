f119053c4b260acf8b3b00f50064f1832c69a542
vinh129150 <vgg8862@gmail.com>
2025-12-16 16:51:52 +0700

    Update BaconHub.lua

--- BaconHub.lua
+++ BaconHub.lua
@@ -1,9 +1,9 @@
 local Games = {
-    [6701277882] = "https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Theforge.lua",
-    [7671049560] = "https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Fishit.lua",
+    [7671049560] = "https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Theforge.lua",
+    [6701277882] = "https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Fishit.lua",
 }
 
-local URL = Games[game.GameId]
+local URL = Games[game.GameId] -- UniverseId
 
 if URL then
     loadstring(game:HttpGet(URL))()
