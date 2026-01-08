7f89353882291f036424855778f9ace735584266
vinh129150 <vgg8862@gmail.com>
2026-01-08 16:53:08 +0700

    Update Bloxfruits.lua

--- Bloxfruits.lua
+++ Bloxfruits.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c4701be4c26f5d455838e714ba61ee47.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/c4701be4c26f5d455838e714ba61ee47.lua"))()
