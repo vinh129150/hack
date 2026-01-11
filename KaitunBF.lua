8dbd20fe5d3a0c7942b7e638f0f84aa9ec966b8e
vinh129150 <vgg8862@gmail.com>
2026-01-11 08:02:40 +0700

    Update KaitunBF.lua

--- KaitunBF.lua
+++ KaitunBF.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/e1ce402c7a198c5f5cec6806b24f15a0.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2f7a809d9390249cc281d66ca41b153f.lua"))()
